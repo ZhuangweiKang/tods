3c0b4aa3f2735ece3c5c8a7b120dbaa258ed45cb
hwy893747147 <hewangyang@yahoo.com>
2021-02-26 10:41:33 -0600

    need to fix MF

--- examples/sk_examples/DeepLog_test.py
+++ examples/sk_examples/DeepLog_test.py
@@ -6,7 +6,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
 #prepare the data
-
 data = np.loadtxt("./500_UCR_Anomaly_robotDOG1_10000_19280_19360.txt")
 
 X_train = np.expand_dims(data[:10000], axis=1)
@@ -30,11 +29,13 @@ y_true = prediction_labels_train
 y_pred = prediction_labels
 
 print('Accuracy Score: ', accuracy_score(y_true, y_pred))
+
 confusion_matrix(y_true, y_pred)
+
 print(classification_report(y_true, y_pred))
 
 precision, recall, thresholds = precision_recall_curve(y_true, y_pred)
-
 f1_scores = 2*recall*precision/(recall+precision)
+
 print('Best threshold: ', thresholds[np.argmax(f1_scores)])
 print('Best F1-Score: ', np.max(f1_scores))
